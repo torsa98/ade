89aa83932a9ae6209d94137f9b8c99a02952187d
torsa98 <adwitiyadeb98@gmail.com>
2020-08-21 23:11:33 +0530

    blog application
    this is a blog application

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+<h1>Welcome to Laravelb</h1>
+<p>This is a blog application.In this application we can write blogs and we can post that and we can edit and delete that posts. Through this application we can create posts and we can style that posts.</p>
+@endsection
